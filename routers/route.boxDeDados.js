cf5529f4708b1d3e28b73d7123ebc9cda71d05ae
Marcus <alpande_1904@hotmail.com>
2024-02-26 19:16:34 -0300

    adicionado clausula where para comparar a data

--- routers/route.boxDeDados.js
+++ routers/route.boxDeDados.js
@@ -4,25 +4,30 @@ import database from '../config/database.js';
 const routeboxDeDados = Router();
 
 routeboxDeDados.get("/box-dados", (req, res) => {
-    let query = `SELECT 
+  const { startDate, endDate } = req.query;
+
+  let query = `SELECT 
                 status_servico,
+                data_servico,
                 COUNT(*) AS total,
                 'R$ ' || FORMAT(SUM(valor), 2) AS total_valor_formatado
                     FROM 
                         tbl_servico
+                    WHERE 
+                        data_servico >= ? AND data_servico <= ?
                     GROUP BY 
                         status_servico ;`;
 
-    //executando a query de select
-    database.db.all(query,[],function(err,rows){
-      if(err){
-        //caso haja erro, retorna o erro
-        return res.status(500).send("Ocorreu algum erro" +err.message);
-      }else{
-        //caso nao haja erro, retorna as linhas da query em formato json
-        return res.status(200).json(rows);
-      }
-    });  
+  // Executando a query de select com os parâmetros startDate e endDate
+  database.db.all(query, [startDate, endDate], function(err, rows) {
+    if (err) {
+      // Caso haja erro, retorna o erro
+      return res.status(500).send("Ocorreu algum erro: " + err.message);
+    } else {
+      // Caso não haja erro, retorna as linhas da query em formato json
+      return res.status(200).json(rows);
+    }
+  });  
 });
 
 export default routeboxDeDados;
