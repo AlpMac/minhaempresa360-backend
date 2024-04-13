e275233cffbe240ff0cd3269f3802e0c6e1ee8d6
Marcus <alpande_1904@hotmail.com>
2024-04-13 17:43:20 -0300

    adicionar servico e itens OK

--- routers/route.AddServico.js
+++ routers/route.AddServico.js
@@ -4,50 +4,62 @@ import Router from 'express';
 import database from '../config/database.js';
 
 const routeAddServico = Router();
-  
-//Middleware para verificar os dados enviados pelo usuario contra injecao de codigo
-  const validateData = [
+
+// Middleware para verificar os dados enviados pelo usuário contra injeção de código
+const validateData = [
     body('data_servico').isDate(),
     body('valor').isNumeric(),
     body('observacao').isString(),
     body('cliente_id').isInt(),
     body('hora_marcada').isString(),
-    body('usuario_id').isInt()
+    body('usuario_id').isInt(),
+    body('itens_contratados.*.id_tbl_tipoItemDoServico').isInt(),
+    body('itens_contratados.*.valorCobrado').isNumeric()
 ];
 
-//metodo post para enviar dados
-routeAddServico.post("/salvar-servico",validateData, (req, res) => {
-
+// método post para enviar dados
+routeAddServico.post("/salvar-servico", validateData, (req, res) => {
     // Verifica se houve erros de validação
     const errors = validationResult(validateData);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors:  errors.array() });
+        return res.status(400).json({ errors: errors.array() });
     }
 
-    let query = `INSERT INTO tbl_servico (data_servico, valor, observacao, cliente_id, hora_marcada, usuario_id,status_servico) 
-    VALUES (?, ?, ?, ?, ? , ?,'A')`;
-//usaremos o req.body para pegar os parametros enviados pelo usuario atraves do corpo da requisicao
-    let parametro = req.body;
+    let query = `INSERT INTO tbl_servico (data_servico, valor, observacao, cliente_id, hora_marcada, usuario_id, status_servico) 
+                 VALUES (?, ?, ?, ?, ?, ?, 'A')`;
+    let params = [
+        req.body.data_servico,
+        req.body.valor,
+        req.body.observacao,
+        req.body.cliente_id,
+        req.body.hora_marcada,
+        req.body.usuario_id
+    ];
 
-    //req.body é desestruturado para extrair os campos necessários para inserção na tabela.
-    //executando a query de select
-    database.db.all(query,[parametro.data_servico, 
-                  parametro.valor,
-                  parametro.observacao,
-                  parametro.cliente_id,
-                  parametro.hora_marcada,
-                  parametro.usuario_id
-                 ],function(err,rows){
-      if(err){
-        //caso haja erro, retorna o erro
-        return res.status(500).send("Ocorreu algum erro" +err.message);
-      }else{
-        //retorno 201 para informar que foi criado com sucesso + mensagem de sucesso
-        return res.status(201).json({ message: 'Serviço inserido com sucesso!' });
-      }
+    database.db.run(query, params, function (err) {
+        if (err) {
+            return res.status(500).send("Ocorreu algum erro" + err.message);
+        }
+      //retorna o id do servico inserido no banco e faz o cadastro dos itens contratados
+        const servicoId = this.lastID;
+        const itensContratados = req.body.itens_servico;
+      
+
+        if (!itensContratados || itensContratados.length === 0) {
+            return res.status(400).json({ message: 'Nenhum item do serviço foi enviado.' });
+        }
+
+        const insertItemQuery = `INSERT INTO tbl_servicoItemContratado (id_tbl_servico, id_tbl_tipoItemDoServico, valor_cobrado) VALUES (?, ?, ?)`;
+        itensContratados.forEach(item => {
+            database.db.run(insertItemQuery, [servicoId, item.codigoItemContratado, item.valorCobrado], function (err) {
+                if (err) {
+                    console.error("Erro ao inserir item do serviço:", err);
+                }
+            });
+        });
+
+        return res.status(201).json({ message: 'Serviço e itens inseridos com sucesso!' });
     });
+});
 
-    
-  });
-
-  export default routeAddServico;
+export default routeAddServico;
